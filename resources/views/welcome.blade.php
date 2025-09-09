e49da9ffe45686739a686f2d74aaacada0efdab1
Manish Manalath <manishmanalath@Manishs-MacBook-Pro.local>
2025-09-09 18:53:52 +0530

    minor fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -118,7 +118,7 @@
                         </li>
                     </ul>
                 </div>
-                <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
+                <div class="bg-[#fff2f2] dark:bg-[#000] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
   <div class="absolute inset-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] flex items-center justify-center">
     <img src="/sevalla_logo.png" class="max-w-[80%] max-h-[80%]" />
   </div>
